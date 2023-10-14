3e8a151f91b25ba99e95c599859f720b47d515da
Hassan Memon <hassan.ghaddai@live.com>
2023-10-14 18:36:56 +0000

    Finish DRF views

--- blog/api_urls.py
+++ blog/api_urls.py
@@ -1,4 +1,9 @@
+#there is no need for this file but I left it
+#  because here is lot of important 
+# learning content, it should be deleted in future
+
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 
 from blog.api_views import post_list, post_detail
 
@@ -6,3 +11,5 @@ urlpatterns = [
   path("posts/", post_list, name="api_post_list"),
   path("posts/<int:pk>", post_detail, name="api_post_detail"),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- blog/api_views.py
+++ blog/api_views.py
@@ -1,3 +1,8 @@
+#there is no need for this file but I left it
+#  because here is lot of important 
+# learning content, it should be deleted in future
+
+
 import json
 from http import HTTPStatus
 
@@ -5,6 +10,9 @@ from django.http import JsonResponse, HttpResponse, HttpResponseNotAllowed
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from blog.models import Post
 from blog.api.serializers import PostSerializer
@@ -26,43 +34,48 @@ from blog.api.serializers import PostSerializer
 
 
 @csrf_exempt
-def post_list(request):
+@api_view(["GET","POST"])
+def post_list(request, format=None):
     if request.method == "GET":
         posts = Post.objects.all()
         # posts_as_dict = [post_to_dict(p) for p in posts]
-        return JsonResponse({"data": PostSerializer(posts, many=True).data})
+        return Response({"data": PostSerializer(posts, many=True).data})
     elif request.method == "POST":
-        post_data = json.loads(request.body)
+        # post_data = json.loads(request.body)
         # post = Post.objects.create(**post_data)
-        serializer = PostSerializer(data=post_data)
-        serializer.is_valid(raise_exception=True)
-        post = serializer.save()
-        return HttpResponse(
-            status=HTTPStatus.CREATED,
-            headers={"Location": reverse("api_post_detail", args=(post.pk,))},
-        )
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+          post = serializer.save()
+          return Response(
+              status=HTTPStatus.CREATED,
+              headers={"Location": reverse("api_post_detail", args=(post.pk,))},
+          )
 
-    return HttpResponseNotAllowed(["GET", "POST"])
+    return Response(serializer.errors, status=HTTPStatus.BAD_REQUEST)
 
 
 @csrf_exempt
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
+@api_view(["GET","PUT", "DELETE"])
+def post_detail(request, pk, format=None):
+    # post = get_object_or_404(Post, pk=pk)
+    try:
+      post = Post.objects.get(pk=pk)
+    except Post.DoesNotExist:
+      return Response(status=HTTPStatus.NOT_FOUND)
 
     if request.method == "GET":
-        return JsonResponse(PostSerializer(post).data)
+        return Response(PostSerializer(post).data)
     elif request.method == "PUT":
-        post_data = json.loads(request.body)
+        # post_data = json.loads(request.body)
         # for field, value in post_data.items():
         #     setattr(post, field, value)
         # post.save()
-        serializer = PostSerializer(post, data=post_data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
+        serializer = PostSerializer(post, data=request.data)
+        if serializer.is_valid():
+          serializer.save()
         
-        return HttpResponse(status=HTTPStatus.NO_CONTENT)
+          return Response(status=HTTPStatus.NO_CONTENT)
+        return Response(serializer.errors, status=HTTPStatus.BAD_REQUEST)
     elif request.method == "DELETE":
         post.delete()
-        return HttpResponse(status=HTTPStatus.NO_CONTENT)
-
-    return HttpResponseNotAllowed(["GET", "PUT", "DELETE"])
+        return Response(status=HTTPStatus.NO_CONTENT)

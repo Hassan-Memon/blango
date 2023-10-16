270b3838a9a1bee1d9a899c043302208821a0afb
Hassan Memon <hassan.ghaddai@live.com>
2023-10-16 03:08:36 +0000

    Finish authentication

--- blango/settings.py
+++ blango/settings.py
@@ -60,6 +60,7 @@ class Dev(Configuration):
         "allauth.socialaccount",
         "allauth.socialaccount.providers.google",
         "rest_framework",
+        "rest_framework.authtoken",
         
     ]
 
@@ -207,9 +208,17 @@ class Dev(Configuration):
     "root": {
         "handlers": ["console"],
         "level": "DEBUG",
-    },
+    }
+  }
 
-}
+    REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+    ]
+  }
+
 
 
 class Prod(Dev):

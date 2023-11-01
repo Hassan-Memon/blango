d35ac5e7ab8ca769353cbd21b89b4f6b405389c7
Hassan Memon <hassan.ghaddai@live.com>
2023-11-01 14:32:25 +0000

    Finish simplejwt

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -7,6 +7,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
 
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 from blog.api.views import (
     # PostList,
     # PostDetail,
@@ -43,6 +45,8 @@ router.register("posts", PostViewSet)
 urlpatterns += [
     path("auth/", include("rest_framework.urls")),
     path("token-auth/", views.obtain_auth_token),
+    path("jwt/", TokenObtainPairView.as_view(), name="jwt_obtain_pair"),
+    path("jwt/refresh/", TokenRefreshView.as_view(), name="jwt_refresh"),
     path("", include(router.urls)),
     path(
       "posts/by-time/<str:period_name>/",

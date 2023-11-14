5603f6a43fc7a571b696dbf64b136684677de93e
Hassan Memon <hassan.ghaddai@live.com>
2023-11-14 14:11:47 +0000

    Finish JavaScript functions

--- blog/static/blog/blog.js
+++ blog/static/blog/blog.js
@@ -1,9 +1,44 @@
-const theNumber = 1
-let yourName = 'Hassan'
+// function sayHello(yourName) {
+//   if (yourName === undefined) {
+//     console.log('Hello, no name')
+//   } else {
+//     console.log("Hello," + yourName)
+//   }
+// }
 
-if (theNumber === 1) {
-  let yourName = 'Memon'
-  alert(yourName)
+// const yourName = 'Hassan Memon'
+
+// console.log("Before setTimeout")
+
+// setTimeout(() => {
+//     sayHello(yourName)
+//   }, 2000
+// )
+
+// console.log('After setTimeout')
+
+for(let i = 0; i < 10; i += 1) {
+  console.log('for loop i: ' + i)
 }
 
-alert(yourName)
+let j = 0
+while(j < 10) {
+  console.log("while loop j: " + j)
+  j += 1
+}
+
+let k = 10
+
+do{
+  console.log("do while loop k: " + k)
+} while(k < 10)
+
+const numbers = [0,1,2,3,4,5,6,7,8,9]
+
+numbers.forEach((value => {
+  console.log("forEach loop value + " + value)
+}))
+
+const double = numbers.map(value => value * 2)
+console.log("map function value")
+console.log(double)
